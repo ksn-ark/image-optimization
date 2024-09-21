614860947d524de2e399485f4e0524841a25fc6f
Nark <sooryarox2004@gmail.com>
2024-09-22 00:19:56 +0300

    init

--- bin/image-optimization.ts
+++ bin/image-optimization.ts
@@ -3,9 +3,10 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ImageOptimizationStack } from '../lib/image-optimization-stack';
 
-
 const app = new cdk.App();
 new ImageOptimizationStack(app, 'ImgTransformationStack', {
-
+  env: {
+    account: '585008083006',
+    region: 'us-east-1',
+  },
 });
-
